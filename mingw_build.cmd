0880db3b610910df692f9e172b14674e90850f66
akzkv <alexey.yu.kazakov@gmail.com>
2021-09-27 23:55:41 +0700

    Added explicit cpack call to .cmd scripts and .yml, added --verbose Added install for both targets, so cpack sees them Disabled cmake action

--- mingw_build.cmd
+++ mingw_build.cmd
@@ -1,2 +1,3 @@
 cmake -G "MinGW Makefiles" -B mingw_build
 cmake --build mingw_build
+cpack -G DEB --config mingw_build/CPackConfig.cmake
--- msvs_build.cmd
+++ msvs_build.cmd
@@ -1,2 +1,4 @@
 cmake -B build
 cmake --build build
+cmake --build build --config Release
+cpack -G WIX --config build/CPackConfig.cmake
